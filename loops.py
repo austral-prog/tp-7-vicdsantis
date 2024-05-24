39e9b70f75bee157279f3b692bf6c4f58f78c49c
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-05-23 23:28:31 -0300

    entrega loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,29 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, lista, indexx):
+    for index, element in enumerate(lista[indexx:], indexx):
+        if element == word:
+            return index
+    return -1
+def index_of_empty(lista):
+    for index, element in enumerate(lista):
+        if element == "":
+            return index
+    return -1
+def index_of(word, lista):
+    for index, element in enumerate(lista):
+        if element == word:
+            return index
+    return -1
+def put(word, lista):
+    for index, element in enumerate(lista):
+        if element == "":
+            lista[index] = word
+            return index
     return -1
 
-
-def index_of_empty(list):
-    return -1
-
-
-def index_of(word, list):
-    return -1
-
-
-def put(word, list):
-    return -1
-
-
-def remove(word, list):
-    return -1
+def remove(word, lista):
+    count = 0
+    for index, element in enumerate(lista):
+        if element == word:
+            lista[index] = ""
+            count += 1
+    return count
