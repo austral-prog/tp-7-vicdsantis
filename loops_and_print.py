20dbe32ee10e678bcbadae974582cf4765359abd
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-05-23 23:29:01 -0300

    entrega loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,17 @@
-def enumerate_list(list):
-    return list
-
-
-def enumerate_backwards(list):
-    return list
+def enumerate_list(strs):
+    result = []
+    count = 0 
+    for string in strs:
+        if string: 
+            result.append(f"{count}. {string}")
+            count += 1
+    return result
+def enumerate_backwards(strs):
+    result = []
+    count = 0  
+    for string in strs:
+        if string: 
+            reversed_string = string[::-1]
+            result.append(f"{count}. {reversed_string}")
+            count += 1
+    return result
